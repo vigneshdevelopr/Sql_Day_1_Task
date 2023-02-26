0e21eace09cd01fa5b6c9daf4c0e53ebc54713ba
vigneshdevelopr <118879122+vigneshdevelopr@users.noreply.github.com>
2023-02-26 23:52:44 +0530

    Add files via upload

---
+++ sql day 1 task.sql
@@ -0,0 +1,20 @@
+-- TABLE
+CREATE TABLE IMDB(
+
+id integer PRIMARY KEY,
+Movie_Name text,
+  Movie_Image VARBINARY,
+  Reviews text, 
+  User text, 
+  Artist text, 
+  Artist_Role text
+  
+
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
